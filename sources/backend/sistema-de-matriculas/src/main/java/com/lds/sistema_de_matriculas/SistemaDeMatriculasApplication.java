82de1b54fd927cf524867a00109d5f4a23261cf9
gramos22 <gabrielrmzf@gmail.com>
2024-08-20 17:07:42 -0300

    initial commit backend

---
+++ sources/backend/sistema-de-matriculas/src/main/java/com/lds/sistema_de_matriculas/SistemaDeMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.lds.sistema_de_matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMatriculasApplication.class, args);
+	}
+
+}
---
+++ sources/backend/sistema-de-matriculas/src/test/java/com/lds/sistema_de_matriculas/SistemaDeMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lds.sistema_de_matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
