2af87664cff0f76d20f3f5981a922225ac77ad83
Júlia <julia.nascimento.1456172@sga.pucminas.br>
2024-09-07 20:09:30 -0300

    Devel: sigin page

--- sources/frontend/enrollment-system/src/Components/Login.jsx
+++ sources/frontend/enrollment-system/src/Components/Login.jsx
@@ -1,23 +1,43 @@
 import { Button, Card, Container, TextField } from "@mui/material";
+import "./Components.css";
+import Typography from "@mui/material/Typography";
+import loginImg from "../img/login-img.png";
 
 const Login = () => {
   return (
     <Card sx={{ width: "30%" }}>
+      <img
+        src={loginImg}
+        alt="loginImg"
+        style={{ width: "60%", marginTop: "32px", marginLeft: "16px" }}
+      />
+      <Typography
+        variant="h3"
+        component="h2"
+        className="Sigin-title"
+        sx={{ py: 2, mt: 2, mb: 2, marginLeft: "32px" }}
+      >
+        Sign in
+      </Typography>
       <Container>
         <TextField
+          sx={{ my: 2 }}
           id="filled-basic"
-          label="User e-mail"
+          label="E-mail"
           variant="filled"
           fullWidth
         />
         <TextField
+          sx={{ my: 2 }}
           id="filled-basic"
-          label="User Password"
+          label="Password"
           variant="filled"
           fullWidth
         />
         <div>
-          <Button variant="contained">Contained</Button>
+          <Button variant="contained" className="Login-btn" sx={{ my: 2 }}>
+            Sign in
+          </Button>
         </div>
       </Container>
     </Card>
